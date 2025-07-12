bfec87baca8123b623f431642dd8000f5f11f593
Divya <55782201+Divya090@users.noreply.github.com>
2025-07-12 17:16:05 +0000

    Added the Flask project in app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("HELLO!")
+from flask import Flask # class
+
+app = Flask(__name__) # object
+
+@app.route('/') # @ - decorator
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
