769d55a063108f5ac1188f05a274acfe01e9111f
Divya <55782201+Divya090@users.noreply.github.com>
2025-07-13 07:06:50 +0000

    Added templates folder

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask # class
+from flask import Flask, render_template # class
 
 app = Flask(__name__) # object
 
 @app.route('/') # @ - decorator
-def hello_world():
-    return 'Hello, World!'
+#def hello_world():
+#    return 'Hello, World!'
+def hello_dd():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
